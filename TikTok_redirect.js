b8c20f99d013177792199de9dee04ef4d2819ebd
Egeon-owo <Eegene8@gmail.com>
2025-07-15 01:09:20 +0800

    Delete TikTok_redirect.js

--- TikTok_redirect.js
+++
@@ -1,8 +0,0 @@
-/**
- * 将所有 safety host 请求，剥离域名后原样重定向
- */
-let original = $request.url.replace(/^https?:\/\/www\.tiktoklinksafety\.com\//, '');
-$done({
-  status: 302,
-  headers: { "Location": original }
-});
